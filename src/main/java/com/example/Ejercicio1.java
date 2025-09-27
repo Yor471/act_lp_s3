23ba57b4af6911d889112f3aca7c1ccff00ecc51
Yor471 <yorladismotatogrisales@gmail.com>
2025-09-27 14:46:51 -0500

    Update Ejercicio1.java

--- src/main/java/com/example/Ejercicio1.java
+++ src/main/java/com/example/Ejercicio1.java
@@ -5,28 +5,22 @@ public class Ejercicio1 {
         System.out.println("Ejercicio 1");
         
 
-        int x = 10;
-        int y = 5;
+        int x = 10, y = 5;
         
         System.out.println("--- Ejercicio 1: Operadores Aritméticos ---");
-
         
         int resultadoSuma = x + y;
         System.out.println("Suma de " + x + " + " + y + " = " + resultadoSuma);
 
-        
         int resultadoResta = x - y;
         System.out.println("Resta de " + x + " - " + y + " = " + resultadoResta);
 
-        
         int resultadoMultiplicacion = x * y;
         System.out.println("Multiplicación de " + x + " * " + y + " = " + resultadoMultiplicacion);
 
-        
         int resultadoDivision = x / y;
         System.out.println("División Entera de " + x + " / " + y + " = " + resultadoDivision);
 
-        
         int resultadoModulo = x % y;
         System.out.println("Módulo/Resto de " + x + " % " + y + " = " + resultadoModulo);
 
