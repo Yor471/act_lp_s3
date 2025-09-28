7b40d0b2f3da995eea2094140ead28bc123f6ac2
Yor471 <yorladismotatogrisales@gmail.com>
2025-09-28 16:43:11 -0500

    Update Ejercicio6.java

--- src/main/java/com/example/Ejercicio6.java
+++ src/main/java/com/example/Ejercicio6.java
@@ -2,7 +2,33 @@ package com.example;
 
 public class Ejercicio6 {
     public static void ejercicio6() {
-        System.out.println("Ejercicio 6");
-        // tu código aquí
+        int edad = 18;
+   System.out.println("verificando edad..");
+   if (edad >= 18) {
+   System.out.println("Edad" + edad + "es mayor de edad ");
+   }
+    double nota = 7.5;
+    System.out.println("verificando nota..");
+    if (nota >= 6.0){
+        System.out.println("nota" + nota + "aprobado");
+    }
+    int  numero = -5;
+    System.out.println("verificando numero..");
+    if (numero > 0){
+    System.out.println("numero positivo");
+    }
+    if (numero<0){ 
+    System.out.println("numero negativo");
+    }
+    if (numero ==0){ 
+    System.out.println("numero es cero");
+
+    }
+}
+}
+
+        
+    
+        
     }
 }
